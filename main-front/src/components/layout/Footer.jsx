63e98abf86632947c5764b0283c6a60223a05a29
CooOnl <yts8007@naver.com>
2024-12-11 17:54:27 +0900

    change

--- main-front/src/components/layout/Footer.jsx
+++ main-front/src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export default function Footer() {
-  return <FooterContainer>HeaderChangeTest</FooterContainer>;
+  return <FooterContainer>HeaderChangeTests</FooterContainer>;
 }
 
 const FooterContainer = styled.div`
